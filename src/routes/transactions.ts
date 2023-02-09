18bb878d25a425f3c67be55ccb42931302eec1ac
José Malty Soares <josemalty@gmail.com>
2023-02-09 19:30:35 -0300

    feat: list all transactions route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,12 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return { transactions }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
